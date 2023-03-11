4749e6a50f3eb148a0305d78a03119540a6f2708
Thomas <thomaskambale1@gmail.com>
2023-03-11 11:33:04 +0300

    adjust stylig for new html layout

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+        var x = document.getElementById("myBtn");
+        x.addEventListener("click", myFunction);
+        x.addEventListener("click", someOtherFunction);
+        function myFunction()
+        {
+            alert ("Wishing you a wonderful day filled with joy, positivity, and endless possibilities!");
+        }
+        function someOtherFunction() 
+        {
+            alert ("Stay Hydrated!");
+        }
+    
+    
